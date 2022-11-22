2938525795685289b1b77fbbb19a06bbd46c23d7
Paulo Marques <paulomarques.rs1@gmail.com>
2022-11-22 01:05:12 -0300

    Correção página Home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Header } from "../../components/Header";
+import { SideBar } from "../../components/SideBar";
 
 export function Home() {
     return (
         <React.Fragment>
             <Header />
+            <SideBar />
         </React.Fragment>
     );
 }
