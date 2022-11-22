9019bd2919ababe3bf4f566c7d6e8699e8259b20
Paulo Marques <paulomarques.rs1@gmail.com>
2022-11-22 04:57:44 -0300

    Inclusão component de card

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,26 @@
+import photos from './photos.json';
+import openImg from '../../assets/images/open.png';
+import favoriteImg from '../../assets/images/favorito.png';
+import fotoImg from '../../assets/images/galeria/foto-1.png';
+import './style.scss';
+import React from 'react';
+
+export function Card() {
+    return (
+        photos.map((photo) => {
+            return (
+                <li key={photo.id} className="cards__item">
+                    <img className="card__image" src={fotoImg} alt="foto" />
+                    <p className='card__description'>{photo.titulo}</p>
+                    <div className='credits'>
+                        <p className='card__description_credits'>{photo.creditos}</p>
+                        <span>
+                            <img className="img__credits" src={favoriteImg} alt="ícone coração de curtir" />
+                            <img className="img__credits" src={openImg} alt="Ícone de abrir modal" />
+                        </span>
+                    </div>
+                </li>
+            )
+        })
+    );        
+}
