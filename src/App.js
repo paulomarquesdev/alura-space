72f3abd7b100c28290dc5651b35b07c3832cf593
Paulo Marques <paulomarques.rs1@gmail.com>
2022-11-22 00:59:33 -0300

    Inclusão do componente Header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import './styles/stylesGlobals.scss'
+import { Home } from "./pages/Home";
 
 export function App() {
   return (
     <>
+      <Home />
     </>
   );
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,7 +4,7 @@ import { Header } from "../../components/Header";
 export function Home() {
     return (
         <React.Fragment>
-            
+            <Header />
         </React.Fragment>
     );
 }
