b9c1aac15a9538a41347748e8c134dea277768c1
Paulo Marques <paulomarques.rs1@gmail.com>
2022-11-22 03:39:11 -0300

    Inclusão tela de aviso para dispositivos móveis

---
+++ src/components/Error/index.jsx
@@ -0,0 +1,11 @@
+import './style.scss';
+
+export default function Error() {
+  return (
+    <div className='error'>
+      <div className='error__container'>
+        <h1>Esse projeto não está portado para dispositivos móveis! Por favor, acesse a partir de um dispositivo desktop.</h1>
+      </div>
+    </div>
+  )
+}
