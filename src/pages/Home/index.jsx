dd4a2578c692493e6e0f8db580d0ba147a059a13
Paulo Marques <paulomarques.rs1@gmail.com>
2022-11-22 06:00:14 -0300

    Update

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,6 +5,7 @@ import { Header } from "../../components/Header";
 import { SideBar } from "../../components/SideBar";
 import { Gallery } from "../../components/Gallery";
 import Error from "../../components/Error";
+import { Aside } from "../../components/Aside";
 
 export function Home() {
     return (
@@ -16,7 +17,10 @@ export function Home() {
                     <Banner />
                 </section>
             </main>
-            <Gallery />
+            <section className="sectionPhotos" >
+                <Gallery />
+                <Aside />
+            </section>
 
             <Error />
         </React.Fragment>
